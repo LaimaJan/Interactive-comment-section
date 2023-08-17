476105e5b8765f7f383a0cf99b480de104583506
LaimaJan <lm.lingyte@gmail.com>
2023-08-17 14:47:11 +0300

    fixed  issue with deleting julius comment

--- src/app/components/Comments/Comments.jsx
+++ src/app/components/Comments/Comments.jsx
@@ -2,6 +2,7 @@ import './Comments.css';
 import CommentsForm from '../CommentForm/CommentsForm';
 import Comment from '../Comment/Comment';
 import { getComments as getCommentsApi, createComment } from '../../../data';
+import DeleteCommentModal from '../DeleteCommentModal/DeleteCommentModal';
 import { useState, useEffect } from 'react';
 
 function Comments({ currentUserId }) {
@@ -39,14 +40,6 @@ function Comments({ currentUserId }) {
 		setDeleteComment(false);
 	};
 
-	const confirmModalDelete = () => {
-		setDeleteComment(true);
-
-		if (deleteComment) {
-			deleteJuliusComment();
-		}
-	};
-
 	const commentModalDelete = (commentId) => {
 		setShowModal(true);
 		setCommentToDeleteId(commentId);
@@ -54,12 +47,14 @@ function Comments({ currentUserId }) {
 
 	const deleteJuliusComment = () => {
 		setShowModal(false);
+
 		const updatedComments = backendComments.filter(
 			(backendComment) => backendComment.id !== commentToDeleteId
 		);
-		console.log('updatedcomments: ', updatedComments);
+
 		setBackendComments(updatedComments);
 		setDeleteComment(false);
+		setCommentToDeleteId(null);
 	};
 
 	const editJuliusComment = (editedText, commentId) => {
@@ -133,23 +128,11 @@ function Comments({ currentUserId }) {
 					juliusWritesComment={'Add a comment ...'}
 				/>
 			</div>
-			{showModal && (
-				<div className="modal">
-					<div className="modal-content">
-						<div className="text-container">
-							<p className="modal-header">Delete comment</p>
-							<p className="modal-text">
-								Are you sure you want to delete this comment? This will remove
-								the comment and can't be undone.
-							</p>
-						</div>
-						<div className="button-container">
-							<button onClick={closeModal}>NO, CANCEL</button>
-							<button onClick={confirmModalDelete}>YES, DELETE</button>
-						</div>
-					</div>
-				</div>
-			)}
+			<DeleteCommentModal
+				isOpen={showModal}
+				onCancel={closeModal}
+				onDelete={deleteJuliusComment}
+			/>
 		</div>
 	);
 }
